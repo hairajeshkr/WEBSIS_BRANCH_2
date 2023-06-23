c610e462534713b046fd093dcf670db4c850a6df
Hemantheg <132075230+Hemantheg@users.noreply.github.com>
2023-06-23 19:06:11 +0530

    Admission,hobbie

---
+++ ERP_CODE/STUDENT/StudentAdmissionDtls.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="StudentAdmissionDtls.aspx.cs" StyleSheetTheme="SkinFile" Inherits="STUDENT_StudentAdmissionDtls" %>
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="ajaxToolkit" %>
+<%@ Register src="../CtrlCommand.ascx" tagname="CtrlCommand" tagprefix="uc1" %>
+<%@ Register src="../CtrlGridList.ascx" tagname="CtrlGridList" tagprefix="uc2" %>
+<%@ Register src="../CtrlDate.ascx" tagname="CtrlDate" tagprefix="uc3" %>
+<%@ Register Src="~/CtrlGridList.ascx" TagPrefix="uc1" TagName="CtrlGridList" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+     <script language="javascript" src="Script/Division.js" type="text/javascript"></script>
+    <div style="height:400px; width:750px">
+      <ajaxToolkit:TabContainer ID="TabContainer1" runat="server" ActiveTabIndex="0" Height="395px" Width="750px" BorderColor="White" BorderStyle="Solid" BorderWidth="0px" style="border:1px solid #fff !important;">
+          <ajaxToolkit:TabPanel runat="server" HeaderText="TabPanel1" ID="TabPanel1">
+              <HeaderTemplate>Education Details
+              </HeaderTemplate>
+              <ContentTemplate>
+                  <div class="result-list" style="overflow: scroll; height: 370px; width: 735px;">
+                      <asp:GridView ID="GrdVwRecords" runat="server" OnPageIndexChanging="GrdVwRecords_PageIndexChanging" OnSelectedIndexChanging="GrdVwRecords_SelectedIndexChanging" SkinID="GrdVwMaster">
+                          <Columns>
+                              <asp:TemplateField HeaderText="Rank">
+                                  <ItemTemplate>
+                                      <asp:LinkButton ID="LnkName" runat="server" CommandName="SELECT" SkinID="LnkBtnGrdMain" Text='<%# Eval("Rank") %>' Width="175px"></asp:LinkButton>
+                                  </ItemTemplate>
+                              </asp:TemplateField>
+                              <asp:TemplateField HeaderText="Admission Class">
+                                  <ItemTemplate>
+                                      <asp:LinkButton ID="LblInstitution" runat="server" CommandName="SELECT" SkinID="LnkBtnGrdMain" Text='<%# Eval("Class") %>' Width="100px"></asp:LinkButton>
+                                  </ItemTemplate>
+                              </asp:TemplateField>
+                              <asp:TemplateField HeaderText="Quota">
+                                  <ItemTemplate>
+                                      <asp:LinkButton ID="LblBoard" runat="server" CommandName="SELECT" SkinID="LnkBtnGrdMain" Text='<%# Eval("Quota") %>' Width="200px"></asp:LinkButton>
+                                  </ItemTemplate>
+                              </asp:TemplateField>
+                              <asp:TemplateField HeaderText="Category">
+                                  <ItemTemplate>
+                                      <asp:LinkButton ID="LblBoard" runat="server" CommandName="SELECT" SkinID="LnkBtnGrdMain" Text='<%# Eval("Category") %>' Width="200px"></asp:LinkButton>
+                                  </ItemTemplate>
+                              </asp:TemplateField>
+                             
+                             
+                          </Columns>
+                      </asp:GridView>
+                  </div>
+              </ContentTemplate>
+          </ajaxToolkit:TabPanel>
+    <ajaxToolkit:TabPanel ID="TabPanel2" runat="server" HeaderText="TabPanel2">
+              <HeaderTemplate>Education Details Register
+              </HeaderTemplate>
+              <ContentTemplate>
+                     <table class="auto-style1">
+                        <tr>
+                          <td class="odd" style="width: 90px; height:35px">
+                            <asp:Label ID="Label21" runat="server" Text="Rank" Width="90px"></asp:Label>
+                          </td>
+                            <td class="odd" style="width: 180px; height: 35px;">
+                              <asp:TextBox ID="TxtRank" runat="server" placeholder="Rank" Width="280px" Height="25px"></asp:TextBox>
+                          </td>
+                            </tr>
+                            <tr>
+                             <td class="odd" style="width: 110px; height: 35px" >
+                             <asp:Label ID="Label22" runat="server" Text="Admission Class" Width="110px"></asp:Label>
+                        </td>
+                         <td class="odd" width="180px" style="height: 30px">
+                               <uc1:CtrlGridList ID="CtrlGrdAdmmisionClass" runat="server" AccountType="ClassList" />
+                          </td>
+                         </tr>
+
+                        <tr>
+
+                            <td class="odd" style="width: 90px; height: 35px" height="30px" >
+                             <asp:Label ID="Label23" runat="server" Text="Quota" Width="90px"></asp:Label>
+                        </td>
+                         <td class="odd" height="30px" style="width: 180px; height: 30px;">
+                               
+                             <uc1:CtrlGridList ID="CtrlGrdQuota" runat="server" PlaceHoldr="Quota"/>
+                               
+                          </td>
+                       </tr>
+                         <tr>
+                             <td class="odd" height="30px" style="width: 90px; height: 30px">
+                              <asp:Label ID="Label24" runat="server" Text="Category" Width="90px"></asp:Label>
+                          </td>
+                           <td class="odd" height="30px" width="180px">
+                               <uc2:CtrlGridList ID="CtrlGrdCategory" runat="server" PlaceHoldr="Category"/>
+                           </td>
+                            
+                           <tr>
+                               <td align="center" class="FooterCommand" colspan="2" valign="middle" height="60px">
+                                   <uc1:CtrlCommand ID="CtrlCommand1" runat="server" IsVisibleClear="True" IsVisibleDelete="True" IsVisibleFind="True" IsVisiblePrint="false" />
+                               </td>
+                        </tr>
+                       </table>
+               </ContentTemplate>
+          </ajaxToolkit:TabPanel>
+           </ajaxToolkit:TabContainer>
+        </div>
+
+
+</asp:Content>
+
---
+++ ERP_CODE/STUDENT/StudentAdmissionDtls.aspx.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+public partial class STUDENT_StudentAdmissionDtls : ClsPageEvents, IPageInterFace
+{
+    ClsStudentAdmissionDetails ObjClsStudAdmm = new ClsStudentAdmissionDetails();
+
+
+    protected override void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            base.Page_Load(sender, e);
+            CtrlCommand1.FooterCommands += new CtrlCommand.ClickEventHandler(ManiPulateDataEvent_Clicked);
+            if (!IsPostBack)
+            {
+                ViewState["STU_ID"] = Request.QueryString["CNTRID"].ToString();
+               // TxtPercentage.Attributes.Add("onkeydown", "return NumbersOnly(event);");
+                FnInitializeForm();
+            }
+        }
+        catch (Exception ex)
+        {
+            FnPopUpAlert(ObjClsStudAdmm.FnAlertMessage(ex.Message));
+        }
+    }
+
+    public override void FnInitializeForm()
+    {
+        TabContainer1.ActiveTabIndex = 0;
+        int iCmpId = FnGetRights().COMPANYID, iBrId = FnGetRights().BRANCHID, iFaId = FnGetRights().FAYEARID, iAcId = FnGetRights().ACYEARID;
+        ObjClsStudAdmm = new ClsStudentAdmissionDetails(ref iCmpId, ref iBrId, ref iFaId, ref iAcId);
+        //ViewState["DT"] = FnGetGeneralTable(ObjCls);
+        Session["DOC"] = "";
+        FnFindRecord();
+    }
+
+
+    public void FnAssignProperty()
+    {
+        base.FnAssignProperty(ObjClsStudAdmm);
+        ObjClsStudAdmm.StudentId = ObjClsStudAdmm.FnIsNumeric(ViewState["STU_ID"].ToString());
+        ObjClsStudAdmm.Rank = TxtRank.Text.Trim();
+        ObjClsStudAdmm.ClassId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdAdmmisionClass.SelectedValue.ToString());
+        ObjClsStudAdmm.QuotaId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdQuota.SelectedValue.ToString());
+        ObjClsStudAdmm.CategoryId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdCategory.SelectedValue.ToString());
+
+    }
+
+    public void FnClose()
+    {
+        throw new NotImplementedException();
+    }
+    public override void FnCancel()
+    {
+        base.FnCancel();
+
+        TxtRank.Text = "";
+        CtrlGrdAdmmisionClass.SelectedValue = "";
+        CtrlGrdQuota.SelectedValue = "";
+        CtrlGrdCategory.SelectedValue = "";
+
+        FnInitializeForm();
+
+        CtrlCommand1.SaveText = "Save";
+        CtrlCommand1.SaveCommandArgument = "NEW";
+        TabContainer1.ActiveTabIndex = 0;
+        FnFocus(TxtRank);
+    }
+
+    public void FnFindRecord()
+    {
+        FnAssignProperty();
+        FnFindRecord(ObjClsStudAdmm);
+        FnGridViewBinding("");
+        TabContainer1.ActiveTabIndex = 0;
+    }
+
+    public object FnGetGridRowCount(string PrmFlag)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FnGridViewBinding(string PrmFlag)
+    {
+        GrdVwRecords.DataSource = ViewState["DT"] as DataTable;
+        GrdVwRecords.DataKeyNames = new String[] { ObjClsStudAdmm.KeyName };
+        GrdVwRecords.DataBind();
+        GrdVwRecords.SelectedIndex = -1;
+    }
+
+    public void FnPrintRecord()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ManiPulateDataEvent_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            switch (((Button)sender).CommandName.ToString().ToUpper())
+            {
+                case "SAVE":
+                    if (TxtRank.Text.Trim().Length <= 0)
+                    {
+                        FnPopUpAlert(ObjClsStudAdmm.FnAlertMessage("Please enter the education"));
+                        FnFocus(TxtRank);
+                        return;
+                    }
+                    FnAssignProperty();
+                    switch (((Button)sender).CommandArgument.ToString().ToUpper())
+                    {
+                        case "NEW":
+                            base.ManiPulateDataEvent_Clicked(((Button)sender).CommandArgument.ToString().ToUpper(), ObjClsStudAdmm, false);
+                            break;
+                        case "UPDATE":
+                            base.ManiPulateDataEvent_Clicked(((Button)sender).CommandArgument.ToString().ToUpper(), ObjClsStudAdmm, false);
+                            break;
+                    }
+                    break;
+                case "DELETE":
+                    FnAssignProperty();
+                    base.ManiPulateDataEvent_Clicked(((Button)sender).CommandName.ToString().ToUpper(), ObjClsStudAdmm, false);
+                    break;
+                case "CLEAR":
+                    //FnPopUpAlert(ObjCls.FnReportWindow("SA.HTML", "wELCOME"));
+                    FnCancel();
+                    break;
+                case "CLOSE":
+                    ObjClsStudAdmm.FnAlertMessage(" You Have No permission To Close Record");
+                    break;
+                case "PRINT":
+                    FnAssignProperty();
+                    base.ManiPulateDataEvent_Clicked(((Button)sender).CommandName.ToString().ToUpper(), ObjClsStudAdmm, false);
+                    break;
+                case "FIND":
+                    FnFindRecord();
+                    break;
+                case "HELP":
+                    ObjClsStudAdmm.FnAlertMessage(" You Have No permission To Help Record");
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            FnPopUpAlert(ObjClsStudAdmm.FnAlertMessage(ex.Message));
+        }
+    }
+
+
+    protected void GrdVwRecords_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+    {
+        try
+        {
+            GrdVwRecords.SelectedIndex = e.NewSelectedIndex;
+            ObjClsStudAdmm.GetDataRow(GrdVwRecords.SelectedDataKey.Values[0].ToString(), ViewState["DT"] as DataTable);
+            ViewState["ID"] = ObjClsStudAdmm.ID.ToString();
+
+            ObjClsStudAdmm.Rank = TxtRank.Text.Trim();
+            ObjClsStudAdmm.ClassId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdAdmmisionClass.SelectedValue.ToString());
+            ObjClsStudAdmm.QuotaId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdQuota.SelectedValue.ToString());
+            ObjClsStudAdmm.CategoryId = ObjClsStudAdmm.FnIsNumeric(CtrlGrdCategory.SelectedValue.ToString());
+
+            TxtRank.Text = ObjClsStudAdmm.Rank.ToString();
+            CtrlGrdAdmmisionClass.SelectedValue = ObjClsStudAdmm.ClassId.ToString();
+            CtrlGrdQuota.SelectedValue = ObjClsStudAdmm.QuotaId.ToString();
+            CtrlGrdCategory.SelectedValue = ObjClsStudAdmm.CategoryId.ToString();
+
+            ViewState["DT_UPDATE"] = ObjClsStudAdmm.UpdateDate.ToString();
+
+            CtrlCommand1.SaveText = "Update";
+            CtrlCommand1.SaveCommandArgument = "UPDATE";
+
+            TabContainer1.ActiveTabIndex = 1;
+        }
+        catch (Exception ex)
+        {
+            FnPopUpAlert(ObjClsStudAdmm.FnAlertMessage(ex.Message));
+        }
+    }
+    protected void GrdVwRecords_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        try
+        {
+            GrdVwRecords.PageIndex = e.NewPageIndex;
+            FnGridViewBinding("");
+        }
+        catch (Exception ex)
+        {
+            FnPopUpAlert(ObjClsStudAdmm.FnAlertMessage(ex.Message));
+        }
+    }
+
+
+}
